ea5ca7d854c993b6a30f768ad797a514fe5461d8
diverss <divers@notgmail.fr>
2026-07-02 14:57:49 +0200

    ajoue vue

--- tasks/admin.py
+++ tasks/admin.py
@@ -7,6 +7,6 @@ admin.site.register(Section)
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['title', 'project', 'section', 'parent', 'priority', 'label', 'completed']
+    list_display = ['title', 'project', 'section', 'parent', 'priority', 'label', 'completed', 'completed_at']
     list_filter = ['project', 'priority', 'completed', 'label']
     search_fields = ['title']
