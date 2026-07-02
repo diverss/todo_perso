eabc2fb3e3aa11e3556efb4302a027224310345a
diverss <divers@notgmail.fr>
2026-07-02 15:00:33 +0200

    bbug

--- tasks/views.py
+++ tasks/views.py
@@ -20,6 +20,7 @@ def _get_inbox():
 
 def _sidebar_context():
     inbox = _get_inbox()
+    print("toto")
     return {
         'projects': Project.objects.filter(is_inbox=False),
         'labels': Label.objects.all(),
