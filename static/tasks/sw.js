efdcd46a1bfb225061c935036d1a4cbf79674310
diverss <divers@notgmail.fr>
2026-07-20 17:55:10 +0200

    ordre des tâches modifibales facilement

--- static/tasks/sw.js
+++ static/tasks/sw.js
@@ -6,7 +6,7 @@
  *   - POST                → laissé passer (géré par pwa.js côté client)
  */
 
-const CACHE = 'todo-v18';
+const CACHE = 'todo-v19';
 
 // Pré-cache minimal (shell de l'app)
 const PRECACHE = [
