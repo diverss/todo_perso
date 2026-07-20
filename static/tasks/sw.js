16f99928ab039176c685694832150562151df572
diverss <divers@notgmail.fr>
2026-07-20 17:19:18 +0200

    plpace du bouton

--- static/tasks/sw.js
+++ static/tasks/sw.js
@@ -6,7 +6,7 @@
  *   - POST                → laissé passer (géré par pwa.js côté client)
  */
 
-const CACHE = 'todo-v12';
+const CACHE = 'todo-v13';
 
 // Pré-cache minimal (shell de l'app)
 const PRECACHE = [
